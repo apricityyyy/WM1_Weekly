4d0104ba9b8bfb959e66e9a0972de8ec2a1328b8
Ilhama Novruzova <inovruzova07@gmail.com>
2023-11-21 16:53:41 +0400

    Finished task 2

--- week10/task02/seasons/src/index.js
+++ week10/task02/seasons/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+function changeColor() {
+  let d = new Date();
+  let hour = d.getHours();
+  console.log(hour);
+
+  if (hour >= 5 && hour <= 12) {
+    return "blue";
+  } else if (hour > 12 && hour < 18) {
+    return "red";
+  } else {
+    return "green";
+  }
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{backgroundColor: changeColor()}}>
+    <h2>Hello there</h2>
+    <p>Welcome to our session</p>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
